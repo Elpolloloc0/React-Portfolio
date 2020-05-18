16fae98641fb294cc011d95fefc978d62f464145
Cieran Brazier <cbrazier@yozu.co.uk>
2020-05-18 23:25:50 +0100

    Matched styles to education

--- src/components/about/styles/AboutStyles.jsx
+++ src/components/about/styles/AboutStyles.jsx
@@ -4,7 +4,8 @@ export const AboutContainer = styled.div`
   display: flex;
   flex: 8;
   flex-direction: column;
-  /* background-color: #FFDBAC; */
+  background-color: #0b0c10;
+  color: #45a29e;
   /* border: 1rem solid red; */
   padding-left: 5rem;
   padding-right: 5rem;
@@ -12,5 +13,4 @@ export const AboutContainer = styled.div`
 
 export const Title = styled.h1`
   font-size: 4rem;
-  font-style: italic;
 `;
--- src/components/work/styles/WorkStyle.jsx
+++ src/components/work/styles/WorkStyle.jsx
@@ -4,13 +4,12 @@ export const WorkContainer = styled.div`
   display: flex;
   flex: 8;
   flex-direction: column;
-  /* background-color: lightblue;
-  border: 1rem solid lawngreen; */
+  background-color: #0b0c10;
+  color: #45a29e;
   padding-left: 5rem;
   padding-right: 5rem;
 `;
 
 export const Title = styled.h1`
   font-size: 4rem;
-  font-style: italic;
 `;
